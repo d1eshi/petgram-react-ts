a341af182a9bb5e0692e3c788a65c75910d0e9e3
d1eshi <silva.diegoF03@gmail.com>
2021-09-08 20:54:10 -0300

    [add] hooks & animations

--- src/components/ListOfCategories/index.tsx
+++ src/components/ListOfCategories/index.tsx
@@ -1,18 +1,54 @@
 import React from 'react'
 
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
 
 import { List, Item } from './style'
 
+interface ICategories {
+  id: number
+  name: string
+  emoji: string
+  cover: string
+  path: string
+}
+
 export function ListOfCategories() {
-  return (
-    <List>
+  const [categories, setCategories] = React.useState<ICategories[]>([])
+
+  const [showFixed, setShowFixed] = React.useState(false)
+
+  React.useEffect(() => {
+    fetch('https://petgram-server-d1eshi.vercel.app/categories')
+      .then(res => res.json())
+      .then(setCategories)
+  }, [])
+
+  React.useEffect(() => {
+    const onScroll = () => {
+      const newShowFixed = window.scrollY > 200
+
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = fixed => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map(category => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
